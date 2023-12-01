994004f9f8b70e78998d4cc2626d1bfbe60b7077
lukix32 <149967673+lukix32@users.noreply.github.com>
2023-12-01 20:44:19 +0100

    Add files via upload

---
+++ gra/src/KeyEventListener.java
@@ -0,0 +1,10 @@
+public interface KeyEventListener {
+    void UpKeyPressed();
+    void DownKeyPressed();
+    void LeftKeyPressed();
+    void RightKeyPressed();
+    void WKeyPressed();
+    void SKeyPressed();
+    void AKeyPressed();
+    void DKeyPressed();
+}
